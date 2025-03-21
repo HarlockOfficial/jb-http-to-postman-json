5f24e1b41bb56ffa114ebc2ea359c03c6734389c
HarlockOfficial <harlockofficial.github@gmail.com>
2025-03-21 18:39:17 +0100

    [edit] output file path to match input path + '/output' [add] error notification on missing params [fix] minor change to iterator variable name [fix] output folder creation

--- jb-env-postman.groovy
+++ jb-env-postman.groovy
@@ -1,5 +1,12 @@
 import groovy.json.JsonOutput
 import groovy.json.JsonSlurper
+import java.nio.file.Files
+import java.nio.file.Path
+
+if (args.length != 1) {
+    println("Usage: jb-env-postman <input-file>")
+    return
+}
 
 def iff = new File(args[0])
 
@@ -16,15 +23,16 @@ ks.each { k ->
     env["_postman_variable_scope"] = "environment"
     def v = jbEnv[k] as Map<String, String>
     v.keySet().each {
-        valit -> {
+        val_it -> {
             def val = [:]
-            val["key"] = valit
-            val["value"] = v[valit]
+            val["key"] = val_it
+            val["value"] = v[val_it]
             val["enabled"] = true
             env["values"].add(val)
         }
     }
-    def off = new File("./${k}.json")
+    def off = new File("${iff.parent}/output/${k}.env.json")
+    Files.createDirectories(Path.of(off.parent))
     off.delete()
     off.createNewFile()
     off << JsonOutput.toJson(env)
